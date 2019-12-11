643c24382cd87014d8d657c0a367005854e1ceb1
falmatad <42183720+falmatad@users.noreply.github.com>
2019-12-10 18:06:54 -0600

    refactored with context api. MVP done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import {ProductContext} from './Context/ProductContext';
+import {CartContext} from './Context/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
 
 function App() {
 	const [products] = useState(data);
@@ -13,28 +16,30 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([
+			...cart,
+			item
+		])
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{cart}}>
+			<Navigation />
 
 			{/* Routes */}
 			<Route
 				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				path="/" component={Products}
 			/>
 
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				component= {ShoppingCart}
 			/>
+			</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
